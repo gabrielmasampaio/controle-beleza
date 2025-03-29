90a50b8f23e7cfcd679cacdca43b88f7962829f8
gabe <gabrielmasampaio@gmail.com>
2025-03-29 17:39:35 -0400

    Fix shoppingListTable.tsx design to remove itens and show price

--- client/components/shoppingListTable.tsx
+++ client/components/shoppingListTable.tsx
@@ -10,7 +10,7 @@ import {
 import {button as buttonStyles} from "@nextui-org/theme";
 import {ShoppingItem} from "@/types";
 import {Link} from "@nextui-org/link";
-import {DeleteIcon, EyeIcon} from "@nextui-org/shared-icons";
+import {CloseIcon, EyeIcon} from "@nextui-org/shared-icons";
 import {Tooltip} from "@nextui-org/tooltip";
 import {User} from "@nextui-org/user";
 import {useDisclosure} from "@nextui-org/use-disclosure";
@@ -66,7 +66,7 @@ export default function ShoppingListTable({items, addQuantityToItem, removeItem}
 
     return (
         <div className="flex flex-col gap-3">
-            <Table aria-label="Tabela de produtos selecionados">
+            <Table aria-label="Tabela de produtos selecionados" >
                 <TableHeader>
                     <TableColumn>Produto</TableColumn>
                     <TableColumn>Preço</TableColumn>
@@ -75,44 +75,61 @@ export default function ShoppingListTable({items, addQuantityToItem, removeItem}
 
                 <TableBody emptyContent={noItemsInList()}>
                     {items.map((item) => (
-                        <TableRow key={item.id}>
+                        <TableRow className="border-b-1 border-default" key={item.id}>
                             <TableCell
                                 className="flex items-center gap-1">
-                                <Tooltip content={<div className="text-xs relative flex items-center gap-2"><EyeIcon/> ver produto </div>}>
-                                    <User onClick={() => handleOpenProduct(item)} className="cursor-pointer hover:opacity-50"
-                                        avatarProps={{radius: "lg", src: item.avatar}}
-                                        name={item.name}
+                                <Tooltip
+                                    content={<div className="text-xs relative flex items-center gap-2"><EyeIcon/> ver
+                                        produto </div>}>
+                                    <User onClick={() => handleOpenProduct(item)}
+                                          className="cursor-pointer hover:opacity-50"
+                                          avatarProps={{radius: "lg", src: item.avatar}}
+                                          name={item.name}
                                     >
                                         {item.name}
                                     </User>
                                 </Tooltip>
                             </TableCell>
-                            <TableCell>R$ {item.price.toFixed(2)}</TableCell>
+                            <TableCell className="text-left">R$ {item.price.toFixed(2)}</TableCell>
                             <TableCell>
-                                <div className="relative flex items-center gap-2">
-                                    <Tooltip color="danger" content="Remover">
-                                      <span onClick={() => handleRemoveProduct(item)}
-                                          className="text-lg text-red-500 cursor-pointer active:opacity-50">
-                                        <DeleteIcon/>
-                                      </span>
-                                    </Tooltip>
-                                        <span className="text-lg cursor-pointer active:opacity-50"
+                                <div className="flex items-center justify-between gap-4 w-full">
+                                    <div className="flex items-center gap-3 justify-center w-full">
+                                        <span
+                                            className="text-lg cursor-pointer active:opacity-50"
                                             onClick={() =>
                                                 item.quantity === 1
                                                     ? handleRemoveProduct(item)
-                                                    : addQuantityToItem(item.id, item.quantity - 1)}
-                                        > - </span>
-                                    <span> {item.quantity} </span>
-                                    <span className="text-lg cursor-pointer active:opacity-50"
-                                        onClick={() => addQuantityToItem(item.id, item.quantity + 1)}> + </span>
+                                                    : addQuantityToItem(item.id, item.quantity - 1)
+                                            }
+                                        >
+                                        –
+                                        </span>
+                                        <span>{item.quantity}</span>
+                                        <span
+                                            className="text-lg cursor-pointer active:opacity-50"
+                                            onClick={() => addQuantityToItem(item.id, item.quantity + 1)}
+                                        >
+                                            +
+                                          </span>
+                                    </div>
+                                    <Tooltip className="ml-2 sm:ml-4" color="danger" content="Remover">
+                                      <span
+                                          onClick={() => handleRemoveProduct(item)}
+                                          className="text-lg text-red-500 cursor-pointer active:opacity-50"
+                                      >
+                                        <CloseIcon/>
+                                      </span>
+                                    </Tooltip>
                                 </div>
                             </TableCell>
                         </TableRow>
                     ))}
                 </TableBody>
             </Table>
-            <ProductModal hideFooter={true} product={selectedItem} isOpen={isProductOpen} onOpenChange={onProductOpenChange}/>
-            <RemoveItemModal  isOpen={isRemoveItemOpen} item={selectedItem} onOpenChange={onRemoveItemOpenChange} onConfirmRemoval={removeItem}/>
+            <ProductModal hideFooter={true} product={selectedItem} isOpen={isProductOpen}
+                          onOpenChange={onProductOpenChange}/>
+            <RemoveItemModal isOpen={isRemoveItemOpen} item={selectedItem} onOpenChange={onRemoveItemOpenChange}
+                             onConfirmRemoval={removeItem}/>
         </div>
     );
 }
