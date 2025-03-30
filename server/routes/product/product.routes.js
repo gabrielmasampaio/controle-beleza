f557d1abf9e20a8958798e6beddfa00b95743c69
gabe <gabrielmasampaio@gmail.com>
2025-03-30 17:27:39 -0400

    Add jwt token middleware to all necessary api endpoints

--- server/routes/product/product.routes.js
+++ server/routes/product/product.routes.js
@@ -2,16 +2,17 @@ const express = require('express');
 const router = express.Router();
 const productController = require('../../controllers/product.controller');
 const {validateMongoId} = require('../../middlewares/mongodb.middleware');
+const {authenticateToken} = require("../../middlewares/auth.middleware");
 const {
     validateProduct,
     validatePartialProduct,
     validateProductQuery
 } = require('../../middlewares/product.middleware');
 
-router.post('/', validateProduct, productController.createProduct);
-router.put('/:id', validateMongoId, validatePartialProduct, productController.updateProduct);
+router.post('/', authenticateToken, validateProduct, productController.createProduct);
+router.put('/:id', authenticateToken, validateMongoId, validatePartialProduct, productController.updateProduct);
 router.get('/', validateProductQuery, productController.getProducts);
 router.get('/:id', validateMongoId, productController.getProductById);
-router.delete('/:id', validateMongoId, productController.deleteProduct);
+router.delete('/:id', authenticateToken, validateMongoId, productController.deleteProduct);
 
 module.exports = router;
--- server/routes/user/user.routes.js
+++ server/routes/user/user.routes.js
@@ -3,11 +3,12 @@ const router = express.Router();
 const userController = require('../../controllers/user.controller');
 const {validateMongoId} = require('../../middlewares/mongodb.middleware');
 const {validateUser, validatePartialUser, validateUserQuery} = require('../../middlewares/user.middleware');
+const {authenticateToken} = require("../../middlewares/auth.middleware");
 
-router.post('/', validateUser, userController.createUser);
-router.put('/:id', validateMongoId, validatePartialUser, userController.updateUser);
-router.get('/', validateUserQuery, userController.getUsers);
-router.get('/:id', validateMongoId, userController.getUserById);
-router.delete('/:id', validateMongoId, userController.deleteUser);
+router.post('/', authenticateToken, validateUser, userController.createUser);
+router.put('/:id', authenticateToken, validateMongoId, validatePartialUser, userController.updateUser);
+router.get('/', authenticateToken, validateUserQuery, userController.getUsers);
+router.get('/:id', authenticateToken, validateMongoId, userController.getUserById);
+router.delete('/:id', authenticateToken, validateMongoId, userController.deleteUser);
 
 module.exports = router;
