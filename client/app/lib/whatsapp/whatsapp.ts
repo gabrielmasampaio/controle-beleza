abb2dfe1b7675adcdc0a12084f30e3fa4b0787aa
gabe <gabrielmasampaio@gmail.com>
2025-03-31 19:36:18 -0400

    Fix whatsapp message formatting to have total

--- client/app/lib/whatsapp/whatsapp.ts
+++ client/app/lib/whatsapp/whatsapp.ts
@@ -1,13 +1,21 @@
 import { ShoppingItem } from "@/types";
+import {formatPrice} from "@/app/lib/text-format";
 
 export function buildWhatsappUrl(items: ShoppingItem[]): string {
     const phone = "5561985951534";
-    const intro = "Olá, gostaria de fazer o pedido dos seguintes produtos:\n\n"; //update to %0A caso nao mude
+    const intro = "Olá, gostaria de fazer o pedido dos seguintes produtos:\n\n";
 
-    const body = items
-        .map((item) => `• ${item.storage}x ${item.name} - ${item.price.toFixed(2).replace('.', ',')} R$`)
+    let total = 0;
+    const itemList = items
+        .map((item) => {
+            total += (item.quantity*item.price)
+            return `• ${item.quantity}x ${item.name} - ${formatPrice(item.price)} R$`
+        })
         .join("\n");
 
-    const message = encodeURIComponent(`${intro}${body}`);
+    const totalLine = `\n\n *• Total = ${formatPrice(total)}*`
+
+    const message = encodeURIComponent(`${intro}${itemList}${totalLine}`);
+    console.log(message)
     return `https://api.whatsapp.com/send?phone=${phone}&text=${message}`;
 }
